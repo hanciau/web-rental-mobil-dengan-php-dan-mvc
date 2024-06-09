d3f52bcc08d346939609a05c6292fa686db1ef1d
hanciau <mfarhanhidayat07@gmail.com>
2024-06-09 18:25:13 +0700

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define('BASEURL', 'http://localhost/pemrogrammanweb/rental_mobil/public');
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'rental_mobil');
+?>
